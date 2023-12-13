b3bab983fc4b230f5bfbd3e830838b15b2673334
David <d4vidmenezes@gmail.com>
2023-12-13 17:49:03 -0300

    model, controller, and pages sources

---
+++ src/controllers/createUser.Controller.js
@@ -0,0 +1,7 @@
+function createPUser(id,name, age, gender, email, password) {
+
+    if(!name || !age || !gender || !email || !password){
+            console.log("Preencha todos os campos!!")
+            return
+    }
+}
---
+++ src/models/User.model.js
@@ -0,0 +1,7 @@
+class User(id, first_name,last_name,email,password){
+    this.id = id
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.password = password
+}
